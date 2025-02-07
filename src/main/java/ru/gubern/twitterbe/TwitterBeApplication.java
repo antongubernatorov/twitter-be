8cb4e2a67bcf6ab10a99625737a5597aee5248bb
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2025-02-07 08:50:41 +0300

    config: initial commit

---
+++ src/main/java/ru/gubern/twitterbe/TwitterBeApplication.java
@@ -0,0 +1,13 @@
+package ru.gubern.twitterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gubern/twitterbe/TwitterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gubern.twitterbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
